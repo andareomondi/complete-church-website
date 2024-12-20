a9bb95fb44b68c52db6de8d14d1f2a76062e2c68
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-12-20 21:03:57 +0300

    Add home view, update URLs, and implement base template with JavaScript functionality

--- church/urls.py
+++ church/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from member.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
